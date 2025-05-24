5ef558d811e63879e693e2d9e15bc764228fe7fd
Luke Richard Goddard <github@lukerichardgoddard.com>
2025-05-24 19:29:10 +1000

    add world boundary and add multiple lines of dialog

--- Scenes/BrownBunny/brown_bunny.gd
+++ Scenes/BrownBunny/brown_bunny.gd
@@ -3,14 +3,23 @@ extends StaticBody2D
 var has_quest: bool = true
 var can_interact: bool = false
 
+@export var dialog_lines: Array[String] = []
+var dialog_index: int = 0
+
 func _ready() -> void:
 	if has_quest:
 		$Exclamation.visible = true
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and can_interact:
-		if $CanvasLayer.visible:
-			$CanvasLayer.visible = false
-		else:
+		if dialog_index < dialog_lines.size():
 			$CanvasLayer.visible = true
+			get_tree().paused = true
+			$CanvasLayer/DialogLabel.text = dialog_lines[dialog_index]
+			dialog_index += 1
+		else:
 			$Exclamation.visible = false
+			$CanvasLayer.visible = false
+			get_tree().paused = false
+			dialog_index = 0
+			
