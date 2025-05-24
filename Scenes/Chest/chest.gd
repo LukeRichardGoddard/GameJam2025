9383117f20bcb32e8f16f981bee6f19c014bb75d
Luke Richard Goddard <github@lukerichardgoddard.com>
2025-05-24 14:24:11 +1000

    remember chest openings between scenes

--- Scenes/Chest/chest.gd
+++ Scenes/Chest/chest.gd
@@ -2,9 +2,12 @@ extends StaticBody2D
 
 var can_interact: bool = false
 var is_open: bool = false
+@export var chest_name: String
 
 func _ready() -> void:
-	$Carrots.visible = false
+	if SceneManager.opened_chests.has(chest_name):
+		is_open = true
+		$AnimatedSprite2D.play("open")
 
 
 func _process(delta: float) -> void:
@@ -17,6 +20,8 @@ func open_chest():
 	$AnimatedSprite2D.play("open")
 	$Carrots.visible = true
 	$Timer.start(3)
+	SceneManager.opened_chests.append(chest_name)
+	print(SceneManager.opened_chests)
 
 
 func _on_timer_timeout() -> void:
--- Scenes/SceneManager/scene_manager.gd
+++ Scenes/SceneManager/scene_manager.gd
@@ -2,8 +2,4 @@ extends Node2D
 
 var player_spawn_position: Vector2
 
-func _ready() -> void:
-	pass
-
-func _process(_delta: float) -> void:
-	pass
+var opened_chests: Array[String] = []
