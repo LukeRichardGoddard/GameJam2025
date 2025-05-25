3f8bf9a3fdbe49cf63f353380db9db492596b137
Luke Richard Goddard <github@lukerichardgoddard.com>
2025-05-25 17:59:22 +1000

    add recurring random bunny movement

--- Scenes/Bunny/bunny.gd
+++ Scenes/Bunny/bunny.gd
@@ -5,6 +5,7 @@ var has_been_fed: bool = false
 var can_interact: bool = false
 var carrot_tween = create_tween().set_loops()
 
+var is_spawning: bool = true
 var spawn_position: Vector2
 
 var destination: Vector2
@@ -15,20 +16,25 @@ func _physics_process(_delta: float) -> void:
 	if destination:
 		dist_to_dest = destination - global_position
 		direction_normal = dist_to_dest.normalized()
-		velocity = direction_normal * 100 * _delta
-		if dist_to_dest.x * dist_to_dest.y < 3:
-			randomiseDestination()
+		if dist_to_dest.x > 2 and dist_to_dest.y > 2:
+			velocity = direction_normal * 500 * _delta
+		else:
+			velocity = Vector2.ZERO
 	else:
 		randomiseDestination()
-	
 	move_and_slide()
 
 func randomiseDestination():
 	randomize()
-	destination = Vector2(randi() % 100 - 50, randi() % 100 - 50)
+	destination = Vector2(randi() % 200 + 50, randi() % 100 - 50)
+	
+func randomisePosition():
+	randomize()
+	position = Vector2(randi() % 200 + 50, randi() % 100 - 50)
 
 func _ready() -> void:
 	carrot_tween.tween_property($NeedsCarrot, "modulate:a", 0, 2).from_current()
+	_on_timer_timeout()
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("interact") and can_interact:
@@ -65,3 +71,9 @@ func feed_bunny():
 	has_been_fed = true
 	if SceneManager.bunny_count == 0:
 		SceneManager.has_quest = true
+
+
+func _on_timer_timeout() -> void:
+	randomiseDestination()
+	$Timer.wait_time = randf() * 3.0
+	$Timer.start()
--- Scenes/GameScene/game_scene.gd
+++ Scenes/GameScene/game_scene.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
+const STARTING_BUNNIES: int = 4
+
 var bunny_scene: PackedScene = preload("res://Scenes/Bunny/bunny.tscn")
 
 func _ready() -> void:
 	if SceneManager.start_game:
-		for i in 3:
+		for i in STARTING_BUNNIES:
 			new_bunny()
-		print(SceneManager.start_game)
 		SceneManager.start_game = false
 	bunny_loader()
 	if SceneManager.music_on:
@@ -19,7 +20,8 @@ func _process(_delta: float) -> void:
 
 func new_bunny():
 	var new_bunny = bunny_scene.instantiate()
-	new_bunny.position = Vector2(randi() % 100 - 50, randi() % 100 - 50)
+	new_bunny.position = Vector2(randi() % 200 + 50, randi() % 100 - 50)
+	new_bunny.spawn_position = new_bunny.position
 	new_bunny.name = "Bunny" + str(SceneManager.bunny_count)
 	SceneManager.bunny_node.add_child(new_bunny)
 	SceneManager.bunny_count += 1
@@ -27,18 +29,15 @@ func new_bunny():
 func bunny_loader():
 	if not SceneManager.bunny_node.get_child_count() == 0:
 		for i in SceneManager.bunny_node.get_children():
-			#i.set_process(true)
 			i.position = i.spawn_position
 			i.reparent($ObstacleLayer/Bunnies)
 	
 func _exit_tree() -> void:
 	if not $ObstacleLayer/Bunnies.get_child_count() == 0:
 		for i in $ObstacleLayer/Bunnies.get_children():
-			#i.set_process(false)
 			i.spawn_position = i.position
 			i.position = Vector2(1000,1000)
 			i.reparent(SceneManager.bunny_node)
-			print(i.name)
 	if SceneManager.music_on:
 		SceneManager.outdoor_music_time = SceneManager.outdoor_music.get_playback_position()
 		SceneManager.outdoor_music.stop()
